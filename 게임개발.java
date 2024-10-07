a4d7ec2a296603b393c4d700930378555127db17
SungMoonPark <taim831@naver.com>
2024-10-07 21:21:57 +0900

    week11

---
+++ 게임개발.java
@@ -0,0 +1,86 @@
+package baekjoon_study;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class 게임개발 {
+	static int N;
+	static long[] answer;
+	static int[] map;
+	static boolean[] visited;
+	static Queue<Integer> q;
+	static List<Integer>[] li;
+	static int[] count;
+	
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+		
+		N = Integer.parseInt(br.readLine());
+		
+		map = new int[N+1];
+		answer = new long[N+1];
+		count = new int[N+1];
+		q = new LinkedList<>();
+
+		li = new ArrayList[N+1];
+		for(int i = 1; i <= N; i++)
+			li[i] = new ArrayList<>();
+		
+		for(int i = 1; i <= N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int num = Integer.parseInt(st.nextToken());
+			answer[i] = num;
+			map[i] = num;
+			
+			int cnt = 0;
+			while(true) {
+				num = Integer.parseInt(st.nextToken());
+				if(num == -1)
+					break;
+				li[num].add(i);
+				count[i]++;
+				cnt++;
+			}		
+			
+			if(cnt == 0)
+				q.offer(i);
+		}
+		
+		visited = new boolean[N+1];
+		check();
+		//System.out.println(Arrays.toString(visited));
+		StringBuilder sb =new StringBuilder();
+		for(int i = 1; i <= N; i++) {
+			sb.append(answer[i]).append("\n");
+		}
+		
+		System.out.println(sb.toString());
+
+	}
+	static void check() {
+		while(!q.isEmpty()) {
+			int num = q.poll();
+			//System.out.println(num);
+			for(int i = 0; i < li[num].size(); i++) {
+				int n = li[num].get(i);
+				count[n]--;
+				answer[n] = Math.max(answer[num] + map[n], answer[n]);
+				if(count[n]==0) {
+					q.offer(n);
+				}
+			}
+			//System.out.println(Arrays.toString(answer));
+			
+		}
+		
+		
+	}
+}
+
